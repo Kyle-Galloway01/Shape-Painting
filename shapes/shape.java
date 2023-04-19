a20bf23bc7a6aad0557634fa8698cf77e5550f91
Kyle Galloway <123207266+Kyle-Galloway01@users.noreply.github.com>
2023-04-19 09:48:16 -0500

    Create shape.java

---
+++ shapes/shape.java
@@ -0,0 +1,26 @@
+
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+public abstract class Shape {
+
+    public static final Color DEFAULTCOLOR = Color.RED;
+    protected Color color; // the data that is common to ALL shapes
+    protected int x;        // x,y is the top left 
+    protected int y;
+
+    // default constructor
+    public Shape() {
+        this.color = DEFAULTCOLOR;
+    }
+
+    public Shape(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = Color.decode(color);
+    }
+
+    abstract public void draw(Graphics g);
+
+}
